5d4a94bce61275034e3b780e18f0713f6dedcf35
savita653 <122449421+savita653@users.noreply.github.com>
2023-03-02 10:42:48 +0530

    first commit

--- resources/views/societies/houses/show.blade.php
+++ resources/views/societies/houses/show.blade.php
@@ -37,6 +37,7 @@
             <td>{{ $house->owners->name }}</td>
             <td>{{ $house->residents->name }}</td>
         </tr>
+        
         @endforeach 
   </table>  
   <td><a href="{{ route('society.index') }}" class="btn btn-primary"> BACK </a></td>
